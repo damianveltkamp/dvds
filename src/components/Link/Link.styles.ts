f59adc29a02d91623ef3e6a0128e200b86d23e47
Damian Veltkamp <damian.veltkamp@deptagency.com>
2022-08-08 20:21:23 +0200

    Set color of Link to secondary

--- src/components/Link/Link.styles.ts
+++ src/components/Link/Link.styles.ts
@@ -1,5 +1,5 @@
 import styled from "@emotion/styled";
 
 export const SC_NextLink = styled("a")`
-  color: ${({ theme }) => `${theme.colors}`};
+  color: ${({ theme }) => `${theme.colors.secondary}`};
 `;
