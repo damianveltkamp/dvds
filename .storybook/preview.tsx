75efbabfa6962888587d7e2108f3f9f0a30d4e5f
Damian Veltkamp <damian.veltkamp@deptagency.com>
2022-08-08 20:20:53 +0200

    Imported theme from theme file

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,6 +1,7 @@
 import * as NextImage from "next/image";
 import { Parameters, DecoratorFn } from "@storybook/react";
 import { ThemeProvider } from "@emotion/react";
+import { theme } from "../src/theme/theme";
 
 const OriginalNextImage = NextImage.default;
 
@@ -8,18 +9,6 @@ Object.defineProperty(NextImage, "default", {
   configurable: true,
   value: (props) => <OriginalNextImage {...props} unoptimized />,
 });
-
-const theme = {
-  colors: {
-    primary: "#1f2937",
-    secondary: "#efa35d",
-    white: "#ffffff",
-    black: "#000000",
-  },
-  borderRadius: {
-    primary: "8px",
-  },
-};
 
 export const parameters: Parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
