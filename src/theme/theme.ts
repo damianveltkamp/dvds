f360ac53fd82700d739f9210d3d8f1394177a1b6
Damian Veltkamp <damian.veltkamp@deptagency.com>
2022-08-08 20:20:40 +0200

    Split up theme into its own dir and file

---
+++ src/theme/theme.ts
@@ -0,0 +1,11 @@
+export const theme = {
+  colors: {
+    primary: "#1f2937",
+    secondary: "#efa35d",
+    white: "#ffffff",
+    black: "#000000",
+  },
+  borderRadius: {
+    primary: "8px",
+  },
+} as const;
