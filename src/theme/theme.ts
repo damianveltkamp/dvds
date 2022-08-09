1f121aaa46d1c512360e9ffea1778b52df7cbceb
Damian Veltkamp <damian.veltkamp@deptagency.com>
2022-08-09 16:19:40 +0200

    Updated theme

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -8,4 +8,33 @@ export const theme = {
   borderRadius: {
     primary: "8px",
   },
+  typography: {
+    heading: {
+      h1: {
+        fontFamily: "Roboto-Bold",
+        fontSize: "2.441rem",
+        lineHeight: "150%",
+      },
+      h2: {
+        fontFamily: "Roboto-Bold",
+        fontSize: "1.953rem",
+        lineHeight: "150%",
+      },
+      h3: {
+        fontFamily: "Roboto-Medium",
+        fontSize: "1.563rem",
+        lineHeight: "150%",
+      },
+      h4: {
+        fontFamily: "Roboto-Medium",
+        fontSize: "1.25rem",
+        lineHeight: "150%",
+      },
+    },
+    paragraphy: {
+      fontFamily: "Roboto-Regular",
+      fontSize: "1rem",
+      lineHeight: "150%",
+    },
+  },
 } as const;
