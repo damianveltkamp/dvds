97729b894e65f876d7d61693452ba7875b808623
Damian Veltkamp <damian.veltkamp@deptagency.com>
2022-08-09 17:09:08 +0200

    Added bare link components

---
+++ src/components/Link/BareExternalLink.tsx
@@ -0,0 +1,19 @@
+import { ReactNode } from "react";
+
+export type BareExternalLinkProps = {
+  href: string;
+  children: ReactNode;
+  className: string;
+};
+
+export const BareExternalLink = ({
+  href,
+  children,
+  className,
+}: BareExternalLinkProps) => {
+  return (
+    <a href={href} rel="noreferrer" target="_blank" className={className}>
+      {children}
+    </a>
+  );
+};
---
+++ src/components/Link/BareLink.tsx
@@ -0,0 +1,16 @@
+import NextLink from "next/link";
+import { ReactNode } from "react";
+
+export type BareLinkProps = {
+  href: string;
+  children: ReactNode;
+  className: string;
+};
+
+export const BareLink = ({ href, children, className }: BareLinkProps) => {
+  return (
+    <NextLink href={href} className={className}>
+      {children}
+    </NextLink>
+  );
+};
