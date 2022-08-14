5bee52caed9ed159053c93ec72288c496e46b0f4
Damian Veltkamp <damian.veltkamp@deptagency.com>
2022-08-14 18:23:03 +0200

    Fixed header component

--- src/components/Header/Header.styles.ts
+++ src/components/Header/Header.styles.ts
@@ -1,10 +1,14 @@
 import styled from "@emotion/styled";
 
 export const SC_Header = styled("header")`
+  padding: 20px;
   background-color: ${({ theme }) => `${theme.colors.DVBlue}`};
 `;
 
 export const SC_ContentContainer = styled("div")`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
   max-width: 700px;
   margin: 0 auto;
   padding-inline: 20px;
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,3 +1,4 @@
+import { GithubBrands, Logo } from "../../icons/components";
 import { SC_ContentContainer, SC_Header } from "./Header.styles";
 
 export type HeaderProps = {};
@@ -5,7 +6,10 @@ export type HeaderProps = {};
 export const Header = ({}: HeaderProps) => {
   return (
     <SC_Header>
-      <SC_ContentContainer>Hey</SC_ContentContainer>
+      <SC_ContentContainer>
+        <Logo size={45} color="white" />
+        <GithubBrands size={35} color="white" />
+      </SC_ContentContainer>
     </SC_Header>
   );
 };
