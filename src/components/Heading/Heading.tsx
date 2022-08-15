a950e34914e8c5f1e5d52f5803b610d14dc589f4
Damian Veltkamp <damian.veltkamp@deptagency.com>
2022-08-15 20:00:46 +0200

    Added id prop to heading component

--- src/components/Heading/Heading.tsx
+++ src/components/Heading/Heading.tsx
@@ -7,20 +7,41 @@ export type HeaderVariants = "black";
 export type HeadingProps = {
   lvl: "h1" | "h2" | "h3" | "h4";
   children: ReactNode;
+  id?: string;
   variant?: HeaderVariants;
 };
 
-export const Heading = ({ lvl, children, variant }: HeadingProps) => {
+export const Heading = ({ lvl, children, id, variant }: HeadingProps) => {
   switch (lvl) {
     case "h1":
-      return <SC_H1 variant={variant}>{children}</SC_H1>;
+      return (
+        <SC_H1 id={id} variant={variant}>
+          {children}
+        </SC_H1>
+      );
     case "h2":
-      return <SC_H2 variant={variant}>{children}</SC_H2>;
+      return (
+        <SC_H2 id={id} variant={variant}>
+          {children}
+        </SC_H2>
+      );
     case "h3":
-      return <SC_H3 variant={variant}>{children}</SC_H3>;
+      return (
+        <SC_H3 id={id} variant={variant}>
+          {children}
+        </SC_H3>
+      );
     case "h4":
-      return <SC_H4 variant={variant}>{children}</SC_H4>;
+      return (
+        <SC_H4 id={id} variant={variant}>
+          {children}
+        </SC_H4>
+      );
     default:
-      return <SC_H2 variant={variant}>{children}</SC_H2>;
+      return (
+        <SC_H2 id={id} variant={variant}>
+          {children}
+        </SC_H2>
+      );
   }
 };
