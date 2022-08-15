45628967a48f21e4668121696c3ea6409255aad5
Damian Veltkamp <damian.veltkamp@deptagency.com>
2022-08-15 21:15:58 +0200

    Updated padding on maincontainer

--- src/components/MainContainer/MainContainer.styles.ts
+++ src/components/MainContainer/MainContainer.styles.ts
@@ -1,7 +1,15 @@
 import styled from "@emotion/styled";
+import media from "../../utils/media";
 
 export const SC_MainContainer = styled("main")`
   max-width: 700px;
   margin: 0 auto;
   padding-inline: 20px;
+  padding-top: 20px;
+  padding-bottom: 40px;
+
+  ${media.min("tablet")} {
+    padding-top: 40px;
+    padding-bottom: 60px;
+  }
 `;
