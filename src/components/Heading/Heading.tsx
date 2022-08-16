d2ca43ca0d19c20bfcc3b5f2955b03341a3a0d90
Damian Veltkamp <damian.veltkamp@deptagency.com>
2022-08-16 16:26:58 +0200

    Added className prop to heading component

--- src/components/Heading/Heading.tsx
+++ src/components/Heading/Heading.tsx
@@ -2,44 +2,51 @@ import { ReactNode } from "react";
 
 import { SC_H1, SC_H2, SC_H3, SC_H4 } from "./Heading.styles";
 
-export type HeaderVariants = "black";
+export type HeaderVariants = "DVBlue" | "black" | undefined;
 
 export type HeadingProps = {
   lvl: "h1" | "h2" | "h3" | "h4";
   children: ReactNode;
   id?: string;
   variant?: HeaderVariants;
+  className?: string;
 };
 
-export const Heading = ({ lvl, children, id, variant }: HeadingProps) => {
+export const Heading = ({
+  lvl,
+  children,
+  id,
+  variant,
+  className,
+}: HeadingProps) => {
   switch (lvl) {
     case "h1":
       return (
-        <SC_H1 id={id} variant={variant}>
+        <SC_H1 id={id} variant={variant} className={className}>
           {children}
         </SC_H1>
       );
     case "h2":
       return (
-        <SC_H2 id={id} variant={variant}>
+        <SC_H2 id={id} variant={variant} className={className}>
           {children}
         </SC_H2>
       );
     case "h3":
       return (
-        <SC_H3 id={id} variant={variant}>
+        <SC_H3 id={id} variant={variant} className={className}>
           {children}
         </SC_H3>
       );
     case "h4":
       return (
-        <SC_H4 id={id} variant={variant}>
+        <SC_H4 id={id} variant={variant} className={className}>
           {children}
         </SC_H4>
       );
     default:
       return (
-        <SC_H2 id={id} variant={variant}>
+        <SC_H2 id={id} variant={variant} className={className}>
           {children}
         </SC_H2>
       );
