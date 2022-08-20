fe3413c84857eeadc126313ff3d178a89d0e829c
Damian Veltkamp <damian.veltkamp@deptagency.com>
2022-08-20 13:16:06 +0200

    Fixed DateLabel component

--- src/components/DateLabel/DateLabel.styles.ts
+++ src/components/DateLabel/DateLabel.styles.ts
@@ -1,10 +1,27 @@
 import styled from "@emotion/styled";
 
-export const SC_DateLabel = styled("time")`
+import { TypeofTheme } from "../../theme/theme";
+import { DateLabelProps, DateLabelVariants } from "./DateLabel";
+
+type SC_DateLabelProps = Pick<DateLabelProps, "variant">;
+
+const colorVariant = (variant: DateLabelVariants, theme: TypeofTheme) => {
+  switch (variant) {
+    case "white":
+      return theme.colors.white;
+    case "black":
+      return theme.colors.black;
+    default:
+      return theme.colors.white;
+  }
+};
+
+export const SC_DateLabel = styled("time")<SC_DateLabelProps>`
   display: block;
   font-family: ${({ theme }) => `${theme.typography.time.fontFamily}`};
   font-size: ${({ theme }) => `${theme.typography.time.fontSize}`};
   line-height: ${({ theme }) => `${theme.typography.time.lineHeight}`};
+  color: ${({ variant, theme }) => colorVariant(variant, theme)};
 
   &:not(:last-child) {
     margin-bottom: 25px;
--- src/components/DateLabel/DateLabel.tsx
+++ src/components/DateLabel/DateLabel.tsx
@@ -2,11 +2,18 @@ import { ReactNode } from "react";
 
 import { SC_DateLabel } from "./DateLabel.styles";
 
+export type DateLabelVariants = "white" | "black" | undefined;
+
 export type DateLabelProps = {
+  variant?: DateLabelVariants;
   children: ReactNode;
   dateTime: string;
 };
 
-export const DateLabel = ({ children, dateTime }: DateLabelProps) => {
-  return <SC_DateLabel dateTime={dateTime}>{children}</SC_DateLabel>;
+export const DateLabel = ({ variant, children, dateTime }: DateLabelProps) => {
+  return (
+    <SC_DateLabel variant={variant} dateTime={dateTime}>
+      {children}
+    </SC_DateLabel>
+  );
 };
