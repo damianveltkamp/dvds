ab48ee9326f7f23f70003a20e5abe9bb9b6f1f48
Damian Veltkamp <damian.veltkamp@deptagency.com>
2022-08-20 14:16:37 +0200

    Updated heading component

--- src/components/Heading/Heading.styles.ts
+++ src/components/Heading/Heading.styles.ts
@@ -32,6 +32,16 @@ export const SC_H1 = styled("h1")<SC_HeadingProps>`
     `${theme.typography.heading.mobile.h1.lineHeight}`};
   color: ${({ variant, theme }) => colorVariant(variant, theme)};
 
+  > a {
+    color: red;
+    font-family: ${({ theme }) =>
+      `${theme.typography.heading.mobile.h1.fontFamily}`};
+    font-size: ${({ theme }) =>
+      `${theme.typography.heading.mobile.h1.fontSize}`};
+    line-height: ${({ theme }) =>
+      `${theme.typography.heading.mobile.h1.lineHeight}`};
+  }
+
   ${media.min("tablet")} {
     font-family: ${({ theme }) =>
       `${theme.typography.heading.desktop.h1.fontFamily}`};
@@ -41,6 +51,7 @@ export const SC_H1 = styled("h1")<SC_HeadingProps>`
       `${theme.typography.heading.desktop.h1.lineHeight}`};
 
     > a {
+      color: blue;
       font-family: ${({ theme }) =>
         `${theme.typography.heading.desktop.h1.fontFamily}`};
       font-size: ${({ theme }) =>
@@ -48,15 +59,6 @@ export const SC_H1 = styled("h1")<SC_HeadingProps>`
       line-height: ${({ theme }) =>
         `${theme.typography.heading.desktop.h1.lineHeight}`};
     }
-  }
-
-  > a {
-    font-family: ${({ theme }) =>
-      `${theme.typography.heading.mobile.h1.fontFamily}`};
-    font-size: ${({ theme }) =>
-      `${theme.typography.heading.mobile.h1.fontSize}`};
-    line-height: ${({ theme }) =>
-      `${theme.typography.heading.mobile.h1.lineHeight}`};
   }
 `;
 
@@ -68,6 +70,15 @@ export const SC_H2 = styled("h2")<SC_HeadingProps>`
   line-height: ${({ theme }) =>
     `${theme.typography.heading.mobile.h2.lineHeight}`};
   color: ${({ variant, theme }) => colorVariant(variant, theme)};
+
+  > a {
+    font-family: ${({ theme }) =>
+      `${theme.typography.heading.mobile.h2.fontFamily}`};
+    font-size: ${({ theme }) =>
+      `${theme.typography.heading.mobile.h2.fontSize}`};
+    line-height: ${({ theme }) =>
+      `${theme.typography.heading.mobile.h2.lineHeight}`};
+  }
 
   ${media.min("tablet")} {
     font-family: ${({ theme }) =>
@@ -86,15 +97,6 @@ export const SC_H2 = styled("h2")<SC_HeadingProps>`
         `${theme.typography.heading.desktop.h2.lineHeight}`};
     }
   }
-
-  > a {
-    font-family: ${({ theme }) =>
-      `${theme.typography.heading.mobile.h2.fontFamily}`};
-    font-size: ${({ theme }) =>
-      `${theme.typography.heading.mobile.h2.fontSize}`};
-    line-height: ${({ theme }) =>
-      `${theme.typography.heading.mobile.h2.lineHeight}`};
-  }
 `;
 
 export const SC_H3 = styled("h3")<SC_HeadingProps>`
@@ -105,6 +107,15 @@ export const SC_H3 = styled("h3")<SC_HeadingProps>`
   line-height: ${({ theme }) =>
     `${theme.typography.heading.mobile.h3.lineHeight}`};
   color: ${({ variant, theme }) => colorVariant(variant, theme)};
+
+  > a {
+    font-family: ${({ theme }) =>
+      `${theme.typography.heading.mobile.h3.fontFamily}`};
+    font-size: ${({ theme }) =>
+      `${theme.typography.heading.mobile.h3.fontSize}`};
+    line-height: ${({ theme }) =>
+      `${theme.typography.heading.mobile.h3.lineHeight}`};
+  }
 
   ${media.min("tablet")} {
     font-family: ${({ theme }) =>
@@ -123,15 +134,6 @@ export const SC_H3 = styled("h3")<SC_HeadingProps>`
         `${theme.typography.heading.desktop.h3.lineHeight}`};
     }
   }
-
-  > a {
-    font-family: ${({ theme }) =>
-      `${theme.typography.heading.mobile.h3.fontFamily}`};
-    font-size: ${({ theme }) =>
-      `${theme.typography.heading.mobile.h3.fontSize}`};
-    line-height: ${({ theme }) =>
-      `${theme.typography.heading.mobile.h3.lineHeight}`};
-  }
 `;
 
 export const SC_H4 = styled("h4")<SC_HeadingProps>`
@@ -142,6 +144,15 @@ export const SC_H4 = styled("h4")<SC_HeadingProps>`
   line-height: ${({ theme }) =>
     `${theme.typography.heading.mobile.h4.lineHeight}`};
   color: ${({ variant, theme }) => colorVariant(variant, theme)};
+
+  > a {
+    font-family: ${({ theme }) =>
+      `${theme.typography.heading.mobile.h4.fontFamily}`};
+    font-size: ${({ theme }) =>
+      `${theme.typography.heading.mobile.h4.fontSize}`};
+    line-height: ${({ theme }) =>
+      `${theme.typography.heading.mobile.h4.lineHeight}`};
+  }
 
   ${media.min("tablet")} {
     font-family: ${({ theme }) =>
@@ -159,14 +170,5 @@ export const SC_H4 = styled("h4")<SC_HeadingProps>`
       line-height: ${({ theme }) =>
         `${theme.typography.heading.desktop.h4.lineHeight}`};
     }
-  }
-
-  > a {
-    font-family: ${({ theme }) =>
-      `${theme.typography.heading.mobile.h4.fontFamily}`};
-    font-size: ${({ theme }) =>
-      `${theme.typography.heading.mobile.h4.fontSize}`};
-    line-height: ${({ theme }) =>
-      `${theme.typography.heading.mobile.h4.lineHeight}`};
   }
 `;
